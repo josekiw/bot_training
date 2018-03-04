e4ff2819466eb817d25e44fa2b11842834dff81e
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-04 16:35:17 +0100

    cambio shard a n57

--- remove.js
+++ remove.js
@@ -10,7 +10,7 @@ module.exports.fetchRemove = function (cb) {
     var request = require("request");
 
     var options = { method: 'POST',
-      url: 'https://dashboard.meraki.com/api/v0/networks/N_595038100766367032/devices/Q2FD-TUKH-CR8V/remove',
+      url: 'https://n57.meraki.com/api/v0/networks/N_595038100766367032/devices/Q2FD-TUKH-CR8V/remove',
       headers: 
        { 'Postman-Token': 'f12c1a69-cdec-408b-a28b-e6323eea8227',
          'Cache-Control': 'no-cache',
