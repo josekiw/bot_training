1974fdfa2e178ffa5c706957b6ef871ac2bbb0e8
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-06 00:55:39 +0100

    ajustamos traffic

--- traffic.js
+++ traffic.js
@@ -6,7 +6,7 @@ var request = require("request");
 
 module.exports.fetchTraffic = function (cb) {
 
-    // List Networks
+    // List Traffic
     var options = {
         method: 'GET',
         url: "https://dashboard.meraki.com/api/v0/networks/L_595038100766328722/traffic",
@@ -16,11 +16,43 @@ module.exports.fetchTraffic = function (cb) {
             "content-type": "application/json"
         }
     };
-
     request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-      
-        bot.reply(body,text);
+
+    if (error) {
+        debug("could not retreive traffic, error: " + error);
+        cb(new Error("Could not retreive current traffic, sorry [Meraki API not responding]"), null, null);
+        return;
+    }
+
+    if ((response < 200) || (response > 299)) {
+        console.log("could not retreive traffic, response: " + response);
+        sparkCallback(new Error("Could not retreive current traffic, sorry [bad anwser from Meraki API]"), null, null);
+        return;
+    }
+
+    var traffic = JSON.parse(body);
+    debug("fetched " + traffic.length + " traffic");
+    fine(JSON.stringify(traffic));
+
+    if (traffic.length == 0) {
+        cb(null, traffic, "Sorry, no traffic on your Meraki Network");
+        return;
+    }
+
+    var nb = traffic.length;
+    var msg = "Analysing Traffic...";
+    if (nb == 1) {
+        msg = "Poor traffic on your Meraki Network...";
+    }
+    for (var i = 0; i < nb; i++) {
+        var current = traffic[i];
+        //msg += "\n:small_blue_diamond: "
+        msg += "\n" + (i+1) + ". ";
+        msg += current.destination + " : " + current.protocol + " / " + current.port;
+    }
+
+    cb(null, traffic, msg);
+
     });
 }
 
