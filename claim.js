96313478c0e177eb67897db3363572f0641712ad
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-09 15:30:43 +0100

    quito el response de function en claim

--- claim.js
+++ claim.js
@@ -17,7 +17,7 @@ module.exports.fetchClaim = function (cb) {
          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
       formData: { serial: 'Q2FD-TUKH-CR8V' } };
     
-      request(options, function (err, response, body) {
+      request(options, function (err, body) {
  //    if (err) {
  //           debug("could not retreive list of devices, error: " + err);
  //           console.log(err);
@@ -25,11 +25,11 @@ module.exports.fetchClaim = function (cb) {
 //            return;
 //        }
 
-        if ((response < 200) || (response > 299)) {
-            console.log("Error <200 o >299: " + response);
-            sparkCallback(new Error("Could not retreive current devices, sorry [bad anwser from Meraki API]"), null, null);
-            return;
-        }
+//        if ((response < 200) || (response > 299)) {
+//            console.log("Error <200 o >299: " + response);
+//            sparkCallback(new Error("Could not retreive current devices, sorry [bad anwser from Meraki API]"), null, null);
+//            return;
+//        }
 
       var claim = JSON.parse(body);
       debug("fetched " + claim.length + " claimed");
