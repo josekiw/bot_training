7d66845ac43caf0933e5ed6a66870b587e11d336
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-09 11:32:08 +0100

    copio tal cual de devices a claim

--- claim.js
+++ claim.js
@@ -17,6 +17,29 @@ module.exports.fetchClaim = function (cb) {
          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
       formData: { serial: 'Q2FD-TUKH-CR8V' } };
     
+      var claim_result = JSON.parse(body);
+      debug("fetched " + claim_result.length + " claimed");
+      fine(JSON.stringify(devices));
+
+      if (claim_result.length == 0) {
+          cb(null, claim_result, "Sorry, no devices claimed on your Meraki ORG");
+          return;
+      }
+
+      var nb = claim_result.length;
+      var msg = "**" + nb + " devices on your Meraki ORG:**";
+      if (nb == 1) {
+          msg = "**only one device is active now:**";
+      }
+      for (var i = 0; i < nb; i++) {
+          var current = claim_result[i];
+          //msg += "\n:small_blue_diamond: "
+          msg += "\n" + (i+1) + ". ";
+          msg += current.model + " - " + current.serial + " - " + current.mac;
+      }
+
+      cb(null, claim_result, msg);
+
     request(options, function (err, response, body) {
       if (err) {
         throw new Error(err);
