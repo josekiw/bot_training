6d4e2789077b32f45477b05fa0a88be06f2dac05
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-09 15:41:27 +0100

    modifico if para success

--- claim.js
+++ claim.js
@@ -42,7 +42,7 @@ module.exports.fetchClaim = function (cb) {
           msg = "Sorry, Device was **already claimed** at API Test - Cliente1";
       }
 
-      else {
+      if (str == "") {
         msg = "Device was **successfully** claimed at API Test - Cliente1";        
       }
 
