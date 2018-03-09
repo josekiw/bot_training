233e72a8d91998811f79b46544ed50ec44a50cce
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-09 14:36:29 +0100

    request.json.response

--- claim.js
+++ claim.js
@@ -52,7 +52,7 @@ module.exports.fetchClaim = function (cb) {
           msg += current.errors;
       }
 
-      cb(null, claim, request.response + "\n\n" + body);
+      cb(null, claim, request.JSON.response + "\n\n" + body);
       console.log(body);
       
     });
