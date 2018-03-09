290d49a8a2631754af4c11445a8ae28a1cb03810
Jose Pla Garcia <joseplagarcia@MacBook-Air-de-J.local>
2018-03-09 11:50:50 +0100

    claim.js error linea 21

--- claim.js
+++ claim.js
@@ -17,7 +17,7 @@ module.exports.fetchClaim = function (cb) {
          'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
       formData: { serial: 'Q2FD-TUKH-CR8V' } };
     
-      request(options, function (err, response, body) {
+      request(options, function (error, response, body) {
         if (error) {
           debug("could not retreive list of devices, error: " + error);
           cb(new Error("Could not retreive current devices, sorry [Meraki API not responding]"), null, null);
